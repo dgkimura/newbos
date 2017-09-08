66cf3d848554cda776d28dc7f5f025e9fbfa1437
David Kimura <dkimura@pivotal.io>
2017-09-08 13:29:22 -0700

    Add error code to isr handler

--- kernel/arch/i386/isr.c
+++ kernel/arch/i386/isr.c
@@ -128,7 +128,9 @@ interrupt_handler(registers_t regs)
     {
         monitor_write("Unhandled exception - ");
         monitor_write_dec(regs.interrupt_number);
-        monitor_write(" : ");
+        monitor_write(" : [errno - ");
+        monitor_write_dec(regs.error_code);
+        monitor_write("] : ");
         monitor_write(exception_messages[regs.interrupt_number]);
         monitor_write("\n");
     }
