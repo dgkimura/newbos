501df04bb431cdd12e45ecf6aaf23c2be1ccf3fa
David Kimura <dkimura@pivotal.io>
2017-09-19 07:44:37 -0700

    Add kmalloc variant functions

--- kernel/arch/i386/mm.c
+++ kernel/arch/i386/mm.c
@@ -4,7 +4,25 @@ extern uint32_t endkernel;
 uint32_t placement_address = (uint32_t)&endkernel;
 
 uint32_t
-kalloc(uint32_t size, int align, uint32_t* physical_address)
+kmalloc_aligned(uint32_t size)
+{
+    return kmalloc_internal(size, 1, 0);
+}
+
+uint32_t
+kmalloc_physical(uint32_t size, uint32_t *physical_address)
+{
+    return kmalloc_internal(size, 0, physical_address);
+}
+
+uint32_t
+kmalloc_aligned_physical(uint32_t size, uint32_t *physical_address)
+{
+    return kmalloc_internal(size, 1, physical_address);
+}
+
+uint32_t
+kmalloc_internal(uint32_t size, int align, uint32_t *physical_address)
 {
     // If the address is not already page-aligned then align it.
     if (1 == align && placement_address & 0xFFFFF000)
--- kernel/arch/i386/mm.h
+++ kernel/arch/i386/mm.h
@@ -3,6 +3,13 @@
 
 #include <stdint.h>
 
-uint32_t kalloc(uint32_t size, int align, uint32_t *physical_address);
+uint32_t kmalloc_aligned(uint32_t size);
+
+uint32_t kmalloc_physical(uint32_t size, uint32_t *physical_address);
+
+uint32_t kmalloc_aligned_physical(uint32_t size, uint32_t *physical_address);
+
+static uint32_t kmalloc_internal(uint32_t size, int align,
+                                 uint32_t *physical_address);
 
 #endif
