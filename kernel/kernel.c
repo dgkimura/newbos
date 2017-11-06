9a4141ffbb24d683bb4d36db9babbd6bd55f6071
David Kimura <dkimura@pivotal.io>
2017-11-06 07:11:24 -0800

    Testing dynamic memory allocation

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,3 +1,4 @@
+#include <newbos/heap.h>
 #include <newbos/timer.h>
 #include <newbos/tty.h>
 
@@ -10,6 +11,18 @@ kernel_main(void)
     asm volatile ("int $0x4");
 
     init_timer(1000);
+
+    uint32_t *a = (uint32_t *)kmalloc(8);
+    uint32_t *b = (uint32_t *)kmalloc(8);
+    monitor_write("a: ");
+    monitor_write_hex(*a);
+    monitor_write("\n");
+    monitor_write("b: ");
+    monitor_write_hex(*b);
+    monitor_write("\n");
+
+    kfree(a);
+    kfree(b);
 
     // Loop forever.
     for (;;);
