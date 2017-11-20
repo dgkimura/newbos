22ca516189dcd5b76ab1f63a3d51b8dd0163ead2
David Kimura <dkimura@pivotal.io>
2017-11-20 12:11:08 -0800

    Fix assember in frame copy function

--- kernel/arch/i386/process.s
+++ kernel/arch/i386/process.s
@@ -25,7 +25,7 @@ copy_page_physical:
     and $0x7fffffff, %edx
     mov %edx, %cr0
 
-    mov 1024, %edx
+    mov $1024, %edx
 
 .loop:
     mov (%ebx), %eax
@@ -34,6 +34,10 @@ copy_page_physical:
     add $4, %ecx
     dec %edx
     jnz .loop
+
+    mov %cr0, %edx
+    or $0x80000000, %edx
+    mov %edx, %cr0
 
     popf
     pop %ebx
