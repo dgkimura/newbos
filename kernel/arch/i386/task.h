f472df30ebdf871f78c8bab24dc35c4c8629c4e4
David Kimura <dkimura@pivotal.io>
2017-11-24 15:53:31 -0800

    Add task structure and function declarations

---
+++ kernel/arch/i386/task.h
@@ -0,0 +1,42 @@
+#ifndef _NEWBOS_TASK_H
+#define _NEWBOS_TASK_H
+
+#include <stdint.h>
+
+#include "paging.h"
+
+typedef struct task
+{
+    int32_t id;
+    uint32_t esp;
+    uint32_t ebp;
+    uint32_t eip;
+    page_directory_t *page_directory;
+} task_t;
+
+void
+init_tasking(
+    void
+);
+
+void
+switch_task(
+    void
+);
+
+int fork(
+    void
+);
+
+void
+move_stack(
+    void *new_stack_start,
+    uint32_t size
+);
+
+int32_t
+getpid(
+    void
+);
+
+#endif
