7afb4ab68d64a555432ff604a4da0818ece5aab6
David Kimura <dkimura@pivotal.io>
2018-02-01 12:11:07 -0800

    Add paging functions

--- kernel/arch/i386/paging.c
+++ kernel/arch/i386/paging.c
@@ -9,7 +9,6 @@ static uint32_t *freemap;
 static uint32_t freemap_frames;
 
 #define FRAME_SIZE (8*sizeof(*freemap))
-#define PAGE_SIZE 0x1000
 #define PAGE_ALIGNMENT 12
 
 void
@@ -65,4 +64,143 @@ frame_free(
     uint32_t frame_offset = page_number % FRAME_SIZE;
     uint32_t frame_mask = (1<<frame_offset);
     freemap[frame_number] |= frame_mask;
+}
+
+struct pagetable *
+pagetable_create(
+    void)
+{
+    struct pagetable *frame = frame_allocate();
+    memset((void *)frame, 0, PAGE_SIZE);
+    return frame;
+}
+
+void
+pagetable_init(
+    struct pagetable *p)
+{
+    unsigned int i;
+
+    for (i=0; i<MAIN_MEMORY_SIZE; i+=PAGE_SIZE)
+    {
+        pagetable_map(p, i, i, PAGE_FLAG_KERNEL|PAGE_FLAG_READWRITE);
+    }
+}
+
+void
+pagetable_alloc(
+    struct pagetable *p,
+    uint32_t virtual_address,
+    uint32_t length,
+    uint32_t flags)
+{
+    uint32_t npages = length / PAGE_SIZE;
+
+    if(length%PAGE_SIZE)
+    {
+        npages += 1;
+    }
+
+    virtual_address &= 0xfffff000;
+
+    while (npages > 0)
+    {
+        uint32_t physical_address;
+        if(!pagetable_get(p, virtual_address, &physical_address))
+        {
+            pagetable_map(p, virtual_address, 0, flags|PAGE_FLAG_ALLOC);
+        }
+        virtual_address += PAGE_SIZE;
+        npages -= 1;
+    }
+}
+
+void
+pagetable_map(
+    struct pagetable *p,
+    uint32_t virtual_address,
+    uint32_t physical_address,
+    uint32_t flags)
+{
+    struct pagetable *q;
+    struct pagetable_entry *e;
+
+    if (flags & PAGE_FLAG_ALLOC)
+    {
+        physical_address = (uint32_t)frame_allocate();
+
+        if(!physical_address)
+        {
+            return;
+        }
+    }
+
+    e = &p->entry[virtual_address>>22];
+
+    if (!e->present)
+    {
+        q = (struct pagetable *)frame_allocate();
+        if (!q)
+        {
+            return;
+        }
+        memset((void *)q, 0, PAGE_SIZE);
+
+        e->present = 1;
+        e->readwrite = 1;
+        e->user = (flags & PAGE_FLAG_KERNEL) ? 0 : 1;
+        e->writethrough = 0;
+        e->nocache = 0;
+        e->accessed = 0;
+        e->dirty = 0;
+        e->pagesize = 0;
+        e->globalpage = (flags & PAGE_FLAG_KERNEL) ? 1 : 0;
+        e->available = 0;
+        e->address = (((uint32_t)q) >> 12);
+    }
+    else
+    {
+        q = (struct pagetable *)(((uint32_t)e->address) << 12);
+    }
+
+    e = &q->entry[(virtual_address>>12) & 0x3FF];
+
+    e->present = 1;
+    e->readwrite = (flags & PAGE_FLAG_READWRITE) ? 1 : 0;
+    e->user = (flags & PAGE_FLAG_KERNEL) ? 0 : 1;
+    e->writethrough = 0;
+    e->nocache = 0;
+    e->accessed = 0;
+    e->dirty = 0;
+    e->pagesize = 0;
+    e->globalpage = !e->user;
+    e->available = (flags & PAGE_FLAG_ALLOC) ? 1 : 0;
+    e->address = (physical_address >> 12);
+}
+
+int
+pagetable_get(
+    struct pagetable *p,
+    uint32_t virtual_address,
+    uint32_t *physical_address)
+{
+    struct pagetable *q;
+    struct pagetable_entry *e;
+
+    e = &p->entry[virtual_address >> 22];
+    if(!e->present)
+    {
+        return 0;
+    }
+
+    q = (struct pagetable*) (e->address << 12);
+
+    e = &q->entry[(virtual_address>>12) & 0x3FF];
+    if(!e->present)
+    {
+        return 0;
+    }
+
+    *physical_address = e->address << 12;
+    return 1;
 }
--- kernel/include/newbos/paging.h
+++ kernel/include/newbos/paging.h
@@ -1,6 +1,36 @@
 #ifndef _NEWBOS_PAGING_H
 #define _NEWBOS_PAGING_H
 
+#define PAGE_SIZE 0x1000
+
+#define PAGE_FLAG_USER        0
+#define PAGE_FLAG_KERNEL      1
+#define PAGE_FLAG_EXISTS      0
+#define PAGE_FLAG_ALLOC       2
+#define PAGE_FLAG_READONLY    0
+#define PAGE_FLAG_READWRITE   4
+#define PAGE_FLAG_NOCLEAR     0
+#define PAGE_FLAG_CLEAR       8
+
+struct pagetable_entry
+{
+    uint32_t present      : 1;
+    uint32_t readwrite    : 1;
+    uint32_t user         : 1;
+    uint32_t writethrough : 1;
+    uint32_t nocache      : 1;
+    uint32_t accessed     : 1;
+    uint32_t dirty        : 1;
+    uint32_t pagesize     : 1;
+    uint32_t globalpage   : 1;
+    uint32_t available    : 3;
+    uint32_t address      : 20;
+};
+
+struct pagetable {
+	struct pagetable_entry entry[PAGE_SIZE/4];
+};
+
 void
 frames_init(
     void
@@ -14,6 +44,34 @@ frame_allocate(
 void
 frame_free(
     void *address
+);
+
+void
+pagetable_init(
+    struct pagetable *p
+);
+
+void
+pagetable_alloc(
+    struct pagetable *p,
+    uint32_t virtual_address,
+    uint32_t length,
+    uint32_t flags
+);
+
+void
+pagetable_map(
+    struct pagetable *p,
+    uint32_t virtual_address,
+    uint32_t physical_address,
+    uint32_t flags
+);
+
+int
+pagetable_get(
+    struct pagetable *p,
+    uint32_t virtual_address,
+    uint32_t *physical_address
 );
 
 #endif
