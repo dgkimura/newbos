54f0f4510a7a6758e2624bdba3ea570cedeb73bd
David Kimura <dkimura@pivotal.io>
2018-03-16 12:47:42 -0700

    Cleanup page frame setup

--- kernel/arch/i386/paging.c
+++ kernel/arch/i386/paging.c
@@ -6,20 +6,40 @@
 
 #include "memory.h"
 
-typedef struct memory_map memory_map_t;
-static uint32_t *freemap;
-static uint32_t freemap_frames;
-
-#define FRAME_SIZE (8*sizeof(*freemap))
-#define PAGE_ALIGNMENT 12
-
 #define MAX_NUM_MEMORY_MAP  100
+#define FOUR_KB     0x1000
+#define PAGING_PL0        0
+#define PAGING_READ_WRITE 1
 
 struct memory_map {
     uint32_t addr;
     uint32_t len;
 };
-static memory_map_t mmap[MAX_NUM_MEMORY_MAP];
+
+struct page_frame_bitmap {
+    uint32_t *start;
+    uint32_t len; /* in bits */
+};
+static struct page_frame_bitmap page_frames;
+static struct memory_map mmap[MAX_NUM_MEMORY_MAP];
+static uint32_t mmap_len;
+
+static uint32_t
+align_up(uint32_t n, uint32_t a)
+{
+    uint32_t m = n % a;
+    if (m == 0)
+    {
+        return n;
+    }
+    return n + (a - m);
+}
+
+static uint32_t
+align_down(uint32_t n, uint32_t a)
+{
+    return n - (n % a);
+}
 
 static uint32_t fill_memory_map(
     uint32_t kernel_physical_start,
@@ -34,7 +54,8 @@ static uint32_t fill_memory_map(
      * Grub multiboot documents that flag[6] bit indicates presense of mmap_*
      * fields set in multiboot structure.
      */
-    if ((multiboot_info->flags & 0x00000040) == 0) {
+    if ((multiboot_info->flags & 0x00000040) == 0)
+    {
         printk("No memory map from GRUB\n");
         return 0;
     }
@@ -42,19 +63,23 @@ static uint32_t fill_memory_map(
     multiboot_memory_map_t *entry =
         (multiboot_memory_map_t *) multiboot_info->mmap_addr;
     while ((uint32_t) entry < multiboot_info->mmap_addr +
-                              multiboot_info->mmap_length) {
-        if (entry->type == MULTIBOOT_MEMORY_AVAILABLE) {
+                              multiboot_info->mmap_length)
+    {
+        if (entry->type == MULTIBOOT_MEMORY_AVAILABLE)
+        {
             addr = (uint32_t) entry->addr;
             len = (uint32_t) entry->len;
             if (addr <= kernel_physical_start
-                    && (addr + len) > kernel_physical_end) {
+                    && (addr + len) > kernel_physical_end)
+            {
 
                 addr = kernel_physical_end;
                 len = len - kernel_physical_end;
 
             }
 
-            if (addr > 0x100000) {
+            if (addr > 0x100000)
+            {
                 mmap[i].addr = addr;
                 mmap[i].len = len;
                 ++i;
@@ -63,11 +88,81 @@ static uint32_t fill_memory_map(
         entry = (multiboot_memory_map_t *)
             (((uint32_t) entry) + entry->size + sizeof(entry->size));
     }
-    printk("addr: %X    len: %X\n", addr, len);
 
     return i;
 }
 
+static uint32_t
+construct_bitmap(struct memory_map *mmap, uint32_t n)
+{
+    uint32_t i, bitmap_pfs, bitmap_size, paddr, vaddr, mapped_mem;
+    uint32_t total_pfs = 0;
+
+    /*
+     * Calculate number of available page frames.
+     */
+    for (i = 0; i < n; ++i)
+    {
+        total_pfs += mmap[i].len / FOUR_KB;
+    }
+
+    bitmap_pfs = div_ceil(div_ceil(total_pfs, 8), FOUR_KB);
+
+    for (i = 0; i < n; ++i)
+    {
+        if (mmap[i].len >= bitmap_pfs * FOUR_KB)
+        {
+            paddr = mmap[i].addr;
+
+            mmap[i].addr += bitmap_pfs * FOUR_KB;
+            mmap[i].len -= bitmap_pfs * FOUR_KB;
+            break;
+        }
+    }
+
+    page_frames.len = total_pfs - bitmap_pfs;
+    bitmap_size = div_ceil(page_frames.len, 8);
+
+    if (i == n)
+    {
+        printk("Couldn't find place for bitmap. bitmap_size: %u\n",
+               bitmap_size);
+        return 1;
+    }
+
+    vaddr = pdt_kernel_find_next_vaddr(bitmap_size);
+    if (vaddr == 0)
+    {
+        printk("Could not find virtual address for bitmap in kernel. "
+               "paddr: %X, bitmap_size: %u, bitmap_pfs: %u\n",
+               paddr, bitmap_size);
+        return 1;
+
+    }
+    printk("bitmap vaddr: %X, bitmap paddr: %X, page_frames.len: %u, "
+           "bitmap_size: %u, bitmap_pfs: %u\n",
+           vaddr, paddr, page_frames.len, bitmap_size, bitmap_pfs);
+
+    mapped_mem = pdt_map_kernel_memory(paddr, vaddr, bitmap_size,
+                                       PAGING_PL0, PAGING_READ_WRITE);
+    if (mapped_mem < bitmap_size) {
+        printk("Could not map kernel memory for bitmap. "
+               "paddr: %X, vaddr: %X, bitmap_size: %u\n",
+               paddr, vaddr, bitmap_size);
+        return 1;
+    }
+
+    page_frames.start = (uint32_t *) vaddr;
+
+    memset(page_frames.start, 0xFF, bitmap_size);
+    uint8_t *last = (uint8_t *)((uint32_t)page_frames.start + bitmap_size - 1);
+    *last = 0;
+    for (i = 0; i < page_frames.len % 8; ++i) {
+        *last |= 0x01 << (7 - i);
+    }
+
+    return 0;
+}
 void
 frames_init(
     uint32_t kernel_physical_start,
@@ -76,210 +171,31 @@ frames_init(
     uint32_t kernel_virtual_end,
     struct multiboot_info *minfo)
 {
+    uint32_t i, addr, len;
+
     printk("Kernel Address:\n");
     printk(" Physical: [%X ... %X]\n",
            kernel_physical_start, kernel_physical_end);
     printk(" Virtual:  [%X ... %X]\n",
            kernel_virtual_start, kernel_virtual_end);
 
-
-    uint32_t n;
-    n = fill_memory_map(
+    mmap_len = fill_memory_map(
         kernel_physical_start,
         kernel_physical_end,
         kernel_virtual_start,
         kernel_virtual_end,
         minfo);
 
-    uint32_t i;
-    uint32_t freemap_bits = MAIN_MEMORY_SIZE/PAGE_SIZE;
-    uint32_t freemap_bytes = freemap_bits / 8;
-    uint32_t freemap_pages = 1 + freemap_bytes / PAGE_SIZE;
+    for (i = 0; i < mmap_len; ++i) {
+        /*
+         * Align addresses on 4KB blocks
+         */
+        addr = align_up(mmap[i].addr, FOUR_KB);
+        len = align_down(mmap[i].len - (addr - mmap[i].addr), FOUR_KB);
 
-    freemap = (void *)MAIN_MEMORY_START;
-
-    freemap_frames = 1 + freemap_bits / FRAME_SIZE;
-
-    memset(freemap, 0xFF, freemap_bytes);
-    for (i=0; i<freemap_pages; i++)
-    {
-        frame_allocate();
+        mmap[i].addr = addr;
+        mmap[i].len = len;
+        printk("mmap[%u] -> addr: %X, len: %u, pfs: %u\n",
+               i, addr, len, len / FOUR_KB);
     }
-}
-
-void *
-frame_allocate(
-    void)
-{
-    uint32_t i, j, frame_mask, page_number;
-    void *address = 0;
-
-    for (i=1; i<freemap_frames; i++)
-    {
-        if (freemap[i] != 0)
-        {
-            for (j=0; j<FRAME_SIZE; j++)
-            {
-                frame_mask = (1<<j);
-                if (freemap[i] & frame_mask)
-                {
-                    freemap[i] &= ~frame_mask;
-                    page_number = i * FRAME_SIZE + j;
-                    address = (page_number << PAGE_ALIGNMENT) +
-                              (void *)MAIN_MEMORY_START;
-                }
-            }
-        }
-    }
-    return address;
-}
-
-void
-frame_free(
-    void *address)
-{
-    uint32_t page_number = ((int32_t)address - MAIN_MEMORY_START) >> PAGE_ALIGNMENT;
-    uint32_t frame_number = page_number / FRAME_SIZE;
-    uint32_t frame_offset = page_number % FRAME_SIZE;
-    uint32_t frame_mask = (1<<frame_offset);
-    freemap[frame_number] |= frame_mask;
-}
-
-struct pagetable *
-pagetable_create(
-    void)
-{
-    struct pagetable *frame = frame_allocate();
-    memset((void *)frame, 0, PAGE_SIZE);
-    return frame;
-}
-
-void
-pagetable_init(
-    struct pagetable *p)
-{
-    unsigned int i;
-
-    for (i=0; i<MAIN_MEMORY_SIZE; i+=PAGE_SIZE)
-    {
-        pagetable_map(p, i, i, PAGE_FLAG_KERNEL|PAGE_FLAG_READWRITE);
-    }
-}
-
-void
-pagetable_alloc(
-    struct pagetable *p,
-    uint32_t virtual_address,
-    uint32_t length,
-    uint32_t flags)
-{
-    uint32_t npages = length / PAGE_SIZE;
-
-    if(length % PAGE_SIZE)
-    {
-        npages += 1;
-    }
-
-    virtual_address &= 0xfffff000;
-
-    while (npages > 0)
-    {
-        uint32_t physical_address;
-        if(!pagetable_get(p, virtual_address, &physical_address))
-        {
-            pagetable_map(p, virtual_address, 0, flags|PAGE_FLAG_ALLOC);
-        }
-        virtual_address += PAGE_SIZE;
-        npages -= 1;
-    }
-}
-
-void
-pagetable_map(
-    struct pagetable *p,
-    uint32_t virtual_address,
-    uint32_t physical_address,
-    uint32_t flags)
-{
-    struct pagetable *q;
-    struct pagetable_entry *e;
-
-    if (flags & PAGE_FLAG_ALLOC)
-    {
-        physical_address = (uint32_t)frame_allocate();
-
-        if(!physical_address)
-        {
-            return;
-        }
-    }
-
-    e = &p->entry[virtual_address>>22];
-
-    if (!e->present)
-    {
-        q = (struct pagetable *)frame_allocate();
-        if (!q)
-        {
-            return;
-        }
-        memset((void *)q, 0, PAGE_SIZE);
-
-        e->present = 1;
-        e->readwrite = 1;
-        e->user = (flags & PAGE_FLAG_KERNEL) ? 0 : 1;
-        e->writethrough = 0;
-        e->nocache = 0;
-        e->accessed = 0;
-        e->dirty = 0;
-        e->pagesize = 0;
-        e->globalpage = (flags & PAGE_FLAG_KERNEL) ? 1 : 0;
-        e->available = 0;
-        e->address = (((uint32_t)q) >> 12);
-    }
-    else
-    {
-        q = (struct pagetable *)(((uint32_t)e->address) << 12);
-    }
-
-    e = &q->entry[(virtual_address>>12) & 0x3FF];
-
-    e->present = 1;
-    e->readwrite = (flags & PAGE_FLAG_READWRITE) ? 1 : 0;
-    e->user = (flags & PAGE_FLAG_KERNEL) ? 0 : 1;
-    e->writethrough = 0;
-    e->nocache = 0;
-    e->accessed = 0;
-    e->dirty = 0;
-    e->pagesize = 0;
-    e->globalpage = !e->user;
-    e->available = (flags & PAGE_FLAG_ALLOC) ? 1 : 0;
-    e->address = (physical_address >> 12);
-}
-
-int
-pagetable_get(
-    struct pagetable *p,
-    uint32_t virtual_address,
-    uint32_t *physical_address)
-{
-    struct pagetable *q;
-    struct pagetable_entry *e;
-
-    e = &p->entry[virtual_address >> 22];
-    if(!e->present)
-    {
-        return 0;
-    }
-
-    q = (struct pagetable*) (e->address << 12);
-
-    e = &q->entry[(virtual_address>>12) & 0x3FF];
-    if(!e->present)
-    {
-        return 0;
-    }
-
-    *physical_address = e->address << 12;
-    return 1;
 }
